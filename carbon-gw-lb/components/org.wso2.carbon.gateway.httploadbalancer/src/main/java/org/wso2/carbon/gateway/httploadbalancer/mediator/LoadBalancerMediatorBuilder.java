da9a743d9ce8a638fa8c0b5b5005e2d857d10fde
Venkat Raman <vraman2811@gmail.com>
2016-05-26 19:04:13 +0530

    Load balancing groups

--- carbon-gw-lb/components/org.wso2.carbon.gateway.httploadbalancer/src/main/java/org/wso2/carbon/gateway/httploadbalancer/mediator/LoadBalancerMediatorBuilder.java
+++ carbon-gw-lb/components/org.wso2.carbon.gateway.httploadbalancer/src/main/java/org/wso2/carbon/gateway/httploadbalancer/mediator/LoadBalancerMediatorBuilder.java
@@ -4,11 +4,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.wso2.carbon.gateway.core.config.GWConfigHolder;
 import org.wso2.carbon.gateway.core.config.ParameterHolder;
+import org.wso2.carbon.gateway.core.flow.Group;
+import org.wso2.carbon.gateway.core.flow.Pipeline;
 import org.wso2.carbon.gateway.httploadbalancer.constants.LoadBalancerConstants;
 import org.wso2.carbon.gateway.httploadbalancer.utils.MapToListConverter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * LoadBalancerMediatorBuilder.
+ * TODO: Resolve Group Issue -> There must be only one group in a config in case of LB.
+ * TODO: If more than one group has to be supported, then OutboundEndpoints should be defined specific to group
  */
 public class LoadBalancerMediatorBuilder {
 
@@ -22,7 +29,21 @@ public class LoadBalancerMediatorBuilder {
                 MapToListConverter.getOutboundEndpointsList(gwConfigHolder.getOutboundEndpoints()),
                 parameterHolder.getParameter(LoadBalancerConstants.ALGORITHM_NAME).getValue());
 
-        gwConfigHolder.getPipeline(gwConfigHolder.getInboundEndpoint().getPipeline()).addMediator(lbMediator);
+        if (gwConfigHolder.hasGroups()) {
+
+            List<Group> groups = new ArrayList<>(gwConfigHolder.getGroups());
+            Pipeline[] pipelines = new Pipeline[groups.size()];
+            for (int i = 0; i < groups.size(); i++) {
+                pipelines[i] = gwConfigHolder.
+                        getPipeline(gwConfigHolder.getGroup(groups.get(i).getPath()).getPipeline());
+                pipelines[i].addMediator(lbMediator);
+            }
+
+        } else {
+
+            gwConfigHolder.
+                    getPipeline(gwConfigHolder.getInboundEndpoint().getPipeline()).addMediator(lbMediator);
+        }
 
 
         return lbMediator;
