428b6206a6430300f163c8f5490a124945010856
Venkat Raman <vraman2811@gmail.com>
2016-06-23 20:37:14 +0530

    added testGetHostAndPort()

--- carbon-gw-lb/components/org.wso2.carbon.gateway.httploadbalancer/src/test/java/org.wso2.carbon.gateway.httploadbalancer/utils/CommonUtilTest.java
+++ carbon-gw-lb/components/org.wso2.carbon.gateway.httploadbalancer/src/test/java/org.wso2.carbon.gateway.httploadbalancer/utils/CommonUtilTest.java
@@ -13,5 +13,18 @@ public class CommonUtilTest {
     public void testGetTimeInMilliSeconds() {
 
         Assert.assertEquals(1, CommonUtil.getTimeInMilliSeconds("1ms"));
+        Assert.assertEquals(9000, CommonUtil.getTimeInMilliSeconds("9s"));
+        Assert.assertEquals(300000, CommonUtil.getTimeInMilliSeconds("5m"));
+        Assert.assertEquals(3600000, CommonUtil.getTimeInMilliSeconds("1h"));
+    }
+
+    @Test
+    public void testGetHostAndPort() {
+
+        Assert.assertEquals("localhost:8080", CommonUtil.getHostAndPort("http://localhost:8080/stockquote/all"));
+        Assert.assertEquals(null, CommonUtil.getHostAndPort("http:/www.google.com"));
+        Assert.assertEquals("www.google.com:80", CommonUtil.getHostAndPort("http://www.google.com"));
+        Assert.assertEquals(null, CommonUtil.getHostAndPort("http://localhost:8080/stockquote\\all"));
+        Assert.assertEquals(null, CommonUtil.getHostAndPort("htp:://localhost:8080/stockquote/all"));
     }
 }
