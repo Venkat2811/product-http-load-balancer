1ff496e4343d03a7aede78fa254c56dbe3627fd8
Venkat2811 <vraman2811@gmail.com>
2016-07-22 20:18:58 +0530

    refactoring

--- performance-benchmark/excecute-tests.sh
+++ performance-benchmark/excecute-tests.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 baseDir=$(dirname "$0")
-concLevels="1 25 50 100 200 400 800"
-# 1600 3200"
+concLevels="1 25 50 100 200 400 800 1600 3200"
 #perTestTime=30
 testLoops=1000
 warmUpConc=200
@@ -14,12 +13,7 @@ timeStmp=$(date +%s)
 
 declare -A MAP
 
-
-#For GW-LB
 service="$1"
-#For Nginx
-#service="http://localhost/stockquote/all"
-
 
 function warmUp(){
 echo "Warmup service.."
